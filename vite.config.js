8f1afc6866e1ab5bd1cec1a07ac8175cfbeaadb4
Justin Forlenza <jforlenza@steamcenter.nyc>
2026-04-16 09:31:14 -0400

    added wrangler support

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import vituum from 'vituum'
 import posthtml from '@vituum/vite-plugin-posthtml'
 import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 /** @type {import('vite').UserConfig} */
 export default {
     plugins: [
         vituum(),
         posthtml({root: './src'}),
-        tailwindcss()
+        tailwindcss(),
+        cloudflare()
     ]
 }
